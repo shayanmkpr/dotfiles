c6852b3baa417e1ef240a76cc914e3319c2ea2b5
Shayan <shayan.malekpour99@gmail.com>
2025-10-04 18:53:06 +0330

    idk

--- nvim/lua/shayan/plugins/colorschemes.lua
+++ nvim/lua/shayan/plugins/colorschemes.lua
@@ -33,17 +33,17 @@ return {
 
 -- ===============================================================
 
-    "rose-pine/neovim",
-    config = function()
-        require("rose-pine").setup({
-            styles = {
-                transparency = true,
-                italic = true,
-                bold = true
-            }
-        })
-        vim.cmd("colorscheme rose-pine-moon")
-    end,
+    -- "rose-pine/neovim",
+    -- config = function()
+    --     require("rose-pine").setup({
+    --         styles = {
+    --             transparency = true,
+    --             italic = true,
+    --             bold = true
+    --         }
+    --     })
+    --     vim.cmd("colorscheme rose-pine-moon")
+    -- end,
 
 -- ===============================================================
 
@@ -66,16 +66,16 @@ return {
 
 -- ===============================================================
 
-    -- "https://gitlab.com/motaz-shokry/gruvbox.nvim",
-    -- name = "gruvbox",
-    -- config = function()
-    --     require("gruvbox").setup({
-    --         styles = {
-    --             transparency = false
-    --         }
-    --     })
-    --     vim.cmd("colorscheme gruvbox-hard")
-    -- end
+    "https://gitlab.com/motaz-shokry/gruvbox.nvim",
+    name = "gruvbox",
+    config = function()
+        require("gruvbox").setup({
+            styles = {
+                transparency = false
+            }
+        })
+        vim.cmd("colorscheme gruvbox")
+    end
 
 -- ===============================================================
 
