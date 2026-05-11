4d9033fc7279a636183567d9db646d340f7b8a20
Shayan <shayan.malekpour99@gmail.com>
2026-05-11 15:19:10 +0330

    new theme and spelling is off

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -99,7 +99,73 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("monokai-pro").setup()
+		require("monokai-pro").setup({
+			transparent_background = true,
+			devicons = true,
+			styles = {
+				comment = { italic = true },
+				keyword = { italic = true },
+				type = { italic = true },
+				storageclass = { italic = true },
+				structure = { italic = true },
+				parameter = { italic = true },
+				annotation = { italic = true },
+				tag_attribute = { italic = true },
+			},
+			filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
+			day_night = {
+				enable = false,
+				day_filter = "ristretto",
+				night_filter = "machine",
+			},
+		})
 		vim.cmd.colorscheme("monokai-pro")
 	end,
 }
+-- require("monokai-pro").setup({
+--   devicons = true,
+--   styles = {
+--     comment = { italic = true },
+--     keyword = { italic = true },
+--     type = { italic = true },
+--     storageclass = { italic = true },
+--     structure = { italic = true },
+--     parameter = { italic = true },
+--     annotation = { italic = true },
+--     tag_attribute = { italic = true },
+--   },
+--   filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+--   day_night = {
+--     enable = false,
+--     day_filter = "pro",
+--     night_filter = "spectrum",
+--   },
+--   inc_search = "background", -- underline | background
+--   background_clear = {
+--     "toggleterm",
+--     "telescope",
+--     "renamer",
+--     "notify",
+--   },
+--   plugins = {
+--     bufferline = {
+--       underline_selected = false,
+--       underline_visible = false,
+--       underline_fill = false,
+--       bold = true,
+--     },
+--     indent_blankline = {
+--       context_highlight = "default", -- default | pro
+--       context_start_underline = false,
+--     },
+--   },
+--   override = function(scheme)
+--     return {}
+--   end,
+--   override_palette = function(filter)
+--     return {}
+--   end,
+--   override_scheme = function(scheme, palette, colors)
+--     return {}
+--   end,
+-- })
